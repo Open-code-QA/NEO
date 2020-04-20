1a9f34969f61aa1ad8027b2834fdc0d9e07d39a2
polinkevichus <62240613+polinkevichus@users.noreply.github.com>
2020-04-20 17:55:01 +0400

    scope = "session"

--- test/authorization_case/test_authorization.py
+++ test/authorization_case/test_authorization.py
@@ -7,7 +7,7 @@ def test_home_button_2527(app):
     app.session.login(username="user1", password="qwe123")
     app.session.open_neo()
     app.session.home_button()
-    assert app.assert_authorization.assert_login() == "Центр боевого управления"
+    assert app.assert_authorization.assert_login() == "Центр боевого управлениz"
 
 
 def test_logout_button_2270(app):
