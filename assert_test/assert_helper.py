27bb84cd422a6028659d93828be1303b5a9942f7
polinkevichus <62240613+polinkevichus@users.noreply.github.com>
2020-04-20 16:02:18 +0400

    Update assert_helper.py

--- assert_test/assert_helper.py
+++ assert_test/assert_helper.py
@@ -3,7 +3,7 @@ class AssertHelper:
     def __init__(self, app):
         self.app = app
 
-    def assert_login(self):
+    def assert_login_313(self):
         wd = self.app.wd
         text_title = wd.find_element_by_xpath("/html/body/div/main/section/div[2]")
         return text_title.text
